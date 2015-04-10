c8c2b2d53235eab111e75a791c3b52929d413b4c
Evan Shimizu <falindrith@gmail.com>
2015-04-10 17:03:16 -0400

    Sampling adjustment

--- source/sceneGen.cpp
+++ source/sceneGen.cpp
@@ -1,10 +1,23 @@
 #include <LumiverseCore.h>
 #include <LumiverseShowControl\LumiverseShowControl.h>
+#include <random>
 
 using namespace Lumiverse;
 using namespace Lumiverse::ShowControl;
 
 string baseDir = "C:/Users/eshimizu/Documents/Lumiverse/attributes/scenes/gen/";
+auto rng = minstd_rand0(1991);
+
+// Set up colors
+//                      R02    R05    R06    R15   R99
+vector<double> warmx = { 0.488, 0.477, 0.457, 0.560, 0.503 };
+vector<double> warmy = { 0.409, 0.399, 0.415, 0.433, 0.404 };
+//                      R360   R51    R3206 D65 R3206 A R54    R62    R71
+vector<double> coolx = { 0.390, 0.291, 0.282,    0.420,  0.424, 0.373, 0.261 };
+vector<double> cooly = { 0.377, 0.286, 0.299,    0.394,  0.377, 0.390, 0.383 };
+//                        R316   R318   R19    R27    R39    R49    R53 D65 R65    R73    R80    R382   R91    R88 D65 R368   R10    R3408  R3206 D65
+vector<double> accentx = { 0.528, 0.568, 0.650, 0.692, 0.584, 0.496, 0.287,  0.283, 0.274, 0.157, 0.189, 0.280, 0.338,  0.249, 0.511, 0.503, 0.282    };
+vector<double> accenty = { 0.412, 0.381, 0.342, 0.302, 0.268, 0.229, 0.302,  0.352, 0.418, 0.209, 0.103, 0.561, 0.417,  0.299, 0.468, 0.414, 0.299    };
 
 // Creates a timeline containing the current state of the rig and nothing else.
 bool createTimeline(Rig* r, Playback* pb, int index) {
@@ -46,7 +59,7 @@ void renderAllScenes(Rig* r, Playback* pb) {
   pb->stop();
 }
 
-int permuteAngles(Rig* r, Playback* pb, int index, vector<string> angles, int steps) {
+int sampleAngles(Rig* r, Playback* pb, int index, vector<string> angles, int steps, float accept) {
   if (angles.size() == 0) {
     return index;
   }
@@ -59,28 +72,34 @@ int permuteAngles(Rig* r, Playback* pb, int index, vector<string> angles, int st
   for (int i = 1; i <= steps; i++) {
     r->select(query).setIntensity((float)(inc * i));
     if (angles.size() > 0) {
-      index = permuteAngles(r, pb, index, angles, steps);
+      index = sampleAngles(r, pb, index, angles, steps, accept);
     }
     else {
-      createTimeline(r, pb, index);
-      index++;
+      if (rng() % 1000 < accept * 1000) {
+        createTimeline(r, pb, index);
+        index++;
+      }
     }
   }
 
   return index;
 }
 
+int permuteAngles(Rig* r, Playback* pb, int index, vector<string> angles, int steps) {
+  return sampleAngles(r, pb, index, angles, steps, 1);
+}
+
 int permute3Point(Rig* r, Playback* pb, int index, int steps) {
-  index = permuteAngles(r, pb, index, { "front left", "top", "front right" }, steps);
+  index = sampleAngles(r, pb, index, { "front left", "top", "front right" }, steps, 0.5);
 
-  r->select("$angle=top").setIntensity(0);
-  index = permuteAngles(r, pb, index, { "front left", "front right" }, steps);
+  //r->select("$angle=top").setIntensity(0);
+  //index = permuteAngles(r, pb, index, { "front left", "front right" }, steps);
   
-  r->select("$angle=front right").setIntensity(0);
-  index = permuteAngles(r, pb, index, { "front left", "top" }, steps);
+  //r->select("$angle=front right").setIntensity(0);
+  //index = permuteAngles(r, pb, index, { "front left", "top" }, steps);
 
-  r->select("$angle=front left").setIntensity(0);
-  index = permuteAngles(r, pb, index, { "top", "front right" }, steps);
+  //r->select("$angle=front left").setIntensity(0);
+  //index = permuteAngles(r, pb, index, { "top", "front right" }, steps);
   
   return index;
 }
@@ -96,15 +115,15 @@ int permuteScene(Rig* r, Playback* pb, int index = 1) {
     createTimeline(r, pb, index);
     index++;
 
-    d->setIntensity(0.75);
-    createTimeline(r, pb, index);
-    index++;
-
-    d->setParam("intensity", 0.5);
+    d->setIntensity(0.50);
     createTimeline(r, pb, index);
     index++;
 
     d->setParam("intensity", 0.2);
+    createTimeline(r, pb, index);
+    index++;
+
+    d->setParam("intensity", 0.1);
     createTimeline(r, pb, index);
     index++;
 
@@ -174,116 +193,59 @@ int permuteScene(Rig* r, Playback* pb, int index = 1) {
     createTimeline(r, pb, index);
     index++;
 
-    left.setIntensity(0.75);
-    right.setIntensity(0.75);
-    top.setIntensity(0);
-    r->select(query).setIntensity(1);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.5);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.2);
-    createTimeline(r, pb, index);
-    index++;
-
-    left.setIntensity(0);
-    right.setIntensity(0.75);
-    top.setIntensity(0.75);
-    r->select(query).setIntensity(1);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.5);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.2);
-    createTimeline(r, pb, index);
-    index++;
-
-    left.setIntensity(0.75);
-    right.setIntensity(0);
-    top.setIntensity(0.75);
-    r->select(query).setIntensity(1);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.5);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.2);
-    createTimeline(r, pb, index);
-    index++;
-
-    left.setIntensity(0);
-    right.setIntensity(0);
-    top.setIntensity(0.75);
-    r->select(query).setIntensity(1);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.5);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.2);
-    createTimeline(r, pb, index);
-    index++;
-
-    left.setIntensity(0);
-    right.setIntensity(0.75);
-    top.setIntensity(0);
-    r->select(query).setIntensity(1);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.5);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.2);
-    createTimeline(r, pb, index);
-    index++;
-
-    left.setIntensity(0.75);
-    right.setIntensity(0);
-    top.setIntensity(0);
-    r->select(query).setIntensity(1);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.5);
-    createTimeline(r, pb, index);
-    index++;
-
-    r->select(query).setIntensity(0.2);
-    createTimeline(r, pb, index);
-    index++;
-
     r->select(query).setIntensity(0);
   }
 
-  // All back
+  // Samples
   r->getAllDevices().setIntensity(0);
-  index = permuteAngles(r, pb, index, { "back", "back left", "back right" }, 3);
+  index = sampleAngles(r, pb, index, { "back", "back left", "back right" }, 3, 0.25);
 
   r->getAllDevices().setIntensity(0);
-  index = permuteAngles(r, pb, index, { "front", "back", "high side left", "high side right" }, 3);
+  index = sampleAngles(r, pb, index, { "front", "back", "high side left", "high side right" }, 3, 0.25);
+  
+  r->getAllDevices().setIntensity(0);
+  index = sampleAngles(r, pb, index, { "top", "front up", "back up" }, 3, 0.25);
 
   r->getAllDevices().setIntensity(0);
-  index = permuteAngles(r, pb, index, { "top", "front up", "back up" }, 3);
-
-  r->getAllDevices().setIntensity(0);
-  index = permuteAngles(r, pb, index, { "front left", "front right", "side left", "side right" }, 3);
+  index = sampleAngles(r, pb, index, { "front left", "front right", "side left", "side right" }, 3, 0.25);
 
   // Special
   // Misc scene configurations that are common and/or interesting
 
   return index;
+}
+
+void pickColors(Rig* r) {
+  // Pick warm in left or right
+  if (rng() % 2 == 0) {
+    int w = rng() % warmx.size();
+    int c = rng() % coolx.size();
+    r->select("$angle=front left").setParam("color", warmx[w], warmy[w]);
+    r->select("$angle=front right").setParam("color", coolx[c], cooly[c]);
+  }
+  else {
+    int w = rng() % warmx.size();
+    int c = rng() % coolx.size();
+    r->select("$angle=front right").setParam("color", warmx[w], warmy[w]);
+    r->select("$angle=front left").setParam("color", coolx[c], cooly[c]);
+  }
+
+  // Pick front
+  int f = rng() % (warmx.size() + coolx.size());
+  if (f < warmx.size()) {
+    r->select("$angle=front").setParam("color", warmx[f], warmy[f]);
+  }
+  else {
+    f -= warmx.size();
+    r->select("$angle=front").setParam("color", coolx[f], cooly[f]);
+  }
+
+  // Pick accents
+  vector<string> angles = { "back", "back left", "back right", "high side left", "high side right", "side left", "side right", "left shin", "right shin", "front up", "back up" };
+  for (const auto& a : angles) {
+    int a = rng() % accentx.size();
+    r->select("$angle=" + a).setParam("color", accentx[a], accenty[a]);
+  }
 }
 
 int main(int argc, char**argv) {
@@ -302,6 +264,7 @@ int main(int argc, char**argv) {
   p->disableContinuousRenderMode();
   p->setSamples(1);
 
+  // Pre-defined color palletes
   r->select("$angle=front left").setParam("color", 0.488, 0.409, 1);
   r->select("$angle=front right").setParam("color", 0.271, 0.290, 1);
   r->select("$angle=front").setParam("color", 0.505, 0.427);
@@ -336,8 +299,13 @@ int main(int argc, char**argv) {
   r->select("$angle=back up").setParam("color", 0.147, 0.333);
   numScenes = permuteScene(r, pb, numScenes);
 
+  for (int i = 0; i < 10; i++) {
+    pickColors(r);
+    numScenes = permuteScene(r, pb, numScenes);
+  }
+
   r->save("C:/Users/eshimizu/Documents/Lumiverse/attributes/scenes/samples.rig.json", true);
   pb->save("C:/Users/eshimizu/Documents/Lumiverse/attributes/scenes/samples.playback.json", true);
-  renderAllScenes(r, pb);
+  // renderAllScenes(r, pb);
   delete r;
 }
